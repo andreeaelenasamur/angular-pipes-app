9f380aee18d6f2f6e32bd1f52842db0a39058ca2
Andreea <andelenasamur@gmail.com>
2023-08-10 17:53:01 +0200

    i18nSelect, i18nPlural and slice pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect
+  public name: string = 'Andreea'
+  public gender: 'male'|'female' = 'female';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void{
+    this.name = 'Fernando';
+    this.gender = 'male';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia']
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 personas esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
 }
