6fb39ea3a0a34cb3b0f1a8f7a24ab89d8075c0a5
Andreea <andelenasamur@gmail.com>
2023-08-10 13:48:05 +0200

    add uppercase, lowercase and titlecase pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'andreea';
+  public nameUpper: string = 'ANDREEA';
+  public fullName: string = 'AndReEa ElEnA';
 }
